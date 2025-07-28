58a36e467f38fa3cb538a86cc5a21c34ce077cbb
Quách Thành Long <stephensouth1307@gmail.com>
2025-07-28 17:19:26 +0700

    - delete

--- The_Next_Generation/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-8">
-      © 2025 The Next Generation Team. All rights reserved.
-    </footer>
-  );
-}
